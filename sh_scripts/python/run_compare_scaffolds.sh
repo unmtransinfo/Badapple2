338f00282dcc34362f95e545a2cb10c3207dd162
Jack-42 <>
2024-07-22 15:25:28 -0600

    organize python sh_scripts

--- sh_scripts/python/run_compare_scaffolds.sh
+++ sh_scripts/python/run_compare_scaffolds.sh
@@ -5,4 +5,4 @@ ba_mol="/media/jack/big_disk/data/badapple/badapple_compounds.tsv"
 ba_scaf="/media/jack/big_disk/data/badapple/badapple_scafs.tsv"
 ba_mol2scaf="/media/jack/big_disk/data/badapple/scaf2cpd.tsv"
 log_out="badapple_compare.out"
-python3 compare_scaffolds.py --log_fname $log_out --rdkit_mol_tsv_file $rd_mol --badapple_mol_tsv_file $ba_mol --rdkit_scaf_tsv_file $rd_scaf --badapple_scaf_tsv_file $ba_scaf --rdkit_mol2scaf_tsv_file $rd_mol2scaf --badapple_mol2scaf_tsv_file $ba_mol2scaf
+python3 src/compare_scaffolds.py --log_fname $log_out --rdkit_mol_tsv_file $rd_mol --badapple_mol_tsv_file $ba_mol --rdkit_scaf_tsv_file $rd_scaf --badapple_scaf_tsv_file $ba_scaf --rdkit_mol2scaf_tsv_file $rd_mol2scaf --badapple_mol2scaf_tsv_file $ba_mol2scaf
--- sh_scripts/run_gen_scaf.sh
+++
@@ -1,7 +0,0 @@
-inp="/media/jack/big_disk/data/badapple/badapple_compounds.tsv"
-max_rings=30
-o_scaf="oscaf.tsv"
-o_mol="omol.tsv"
-o_mol2scaf="omol2scaf.tsv"
-log_fname="gen_scaf_log.out"
-python generate_scaffolds.py --log_fname $log_fname --i $inp --iheader --max_rings $max_rings --o_mol $o_mol --o_scaf $o_scaf --o_mol2scaf $o_mol2scaf --smiles_column 1 --name_column 0
