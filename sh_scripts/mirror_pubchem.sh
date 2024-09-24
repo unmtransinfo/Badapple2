2c2ccc71d6611e3f8a3ea80cbaf4ecab26d4a761
Jack-42 <johnjack0987@gmail.com>
2024-09-24 14:33:35 -0600

    update mirror_pubchem.sh, include extras

--- sh_scripts/mirror_pubchem.sh
+++ sh_scripts/mirror_pubchem.sh
@@ -1,22 +1,35 @@
 # based on: https://depth-first.com/articles/2010/02/08/big-data-in-chemistry-mirroring-pubchem-the-easy-way/
 
 workdir="/media/jack/big_disk/data/pubchem"
-cd $workdir || exit
+ftp_directory="ftp.ncbi.nlm.nih.gov/pubchem/"
+
+# Function to sync files from FTP to local directory
+sync_files() {
+    local ftp_dir=$1
+    local src_dir=$2
+
+    if [ -d "$ftp_dir/$src_dir" ]; then
+        rsync -r -t -v --progress --bwlimit=5000 --include='*.zip' --include='*.gz' --exclude='*' "$ftp_dir/$src_dir/" "$src_dir/"
+    else
+        echo "Source directory does not exist. Exiting."
+        exit 1
+    fi
+}
+
+# Change to the working directory
+cd "$workdir" || exit
 
 # Create necessary directories
-mkdir -p ftp.ncbi.nlm.nih.gov/pubchem
-mkdir -p bioassay
+mkdir -p "$ftp_directory"
+mkdir -p Bioassay/CSV/Data/
+mkdir -p Bioassay/Extras/
 
 # Mount the FTP directory using curlftpfs
-curlftpfs ftp.ncbi.nlm.nih.gov/pubchem/ ftp.ncbi.nlm.nih.gov/pubchem/ || { echo "Failed to mount FTP"; exit 1; }
+curlftpfs "$ftp_directory" "$ftp_directory" || { echo "Failed to mount FTP"; exit 1; }
 
-# Check if the directory exists before rsync
-if [ -d "ftp.ncbi.nlm.nih.gov/pubchem/Bioassay/CSV/Data" ]; then
-    rsync -r -t -v --progress --bwlimit=5000 --include='*.zip' --exclude='*' ftp.ncbi.nlm.nih.gov/pubchem/Bioassay/CSV/Data/ bioassay/
-else
-    echo "Source directory does not exist. Exiting."
-    exit 1
-fi
+# Sync files
+sync_files "$ftp_directory" "Bioassay/CSV/Data/"
+sync_files "$ftp_directory" "Bioassay/Extras/"
 
 # Unmount the FTP directory
-fusermount -u ftp.ncbi.nlm.nih.gov/pubchem/
+fusermount -u $ftp_directory
