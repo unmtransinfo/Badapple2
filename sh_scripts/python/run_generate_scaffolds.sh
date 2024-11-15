57ba7c13c4b1ef5d45a98c88a68b09930fe5481b
Jack-42 <johnjack0987@gmail.com>
2024-11-15 10:51:49 -0700

    generate scaffolds script update

--- sh_scripts/python/run_generate_scaffolds.sh
+++ sh_scripts/python/run_generate_scaffolds.sh
@@ -1,8 +1,11 @@
-inp="/media/jack/big_disk/data/badapple/badapple_compounds.tsv"
-out_dir="/media/jack/big_disk/data/badapple2"
+inp="/media/jack/big_disk/data/badapple/badapple2/cpds_set.tsv"
+out_dir="/media/jack/big_disk/data/badapple/badapple2"
 max_rings=5
 o_scaf="$out_dir/scafs.tsv"
 o_mol="$out_dir/cpds.tsv"
 o_mol2scaf="$out_dir/scaf2cpd.tsv"
-log_fname="$out_dir/gen_scaf.log"
-python src/generate_scaffolds.py --log_fname $log_fname --i $inp --iheader --max_rings $max_rings --o_mol $o_mol --o_scaf $o_scaf --o_mol2scaf $o_mol2scaf --smiles_column 1 --name_column 0
+log_fname="$out_dir/gen_scaf_log.out"
+python src/generate_scaffolds.py \
+    --log_fname $log_fname --i $inp --max_rings $max_rings \
+    --o_mol $o_mol --o_scaf $o_scaf --o_mol2scaf $o_mol2scaf \
+    --smiles_column 1 --name_column 0
