4bd920cb90145b9abfaf3d1a5de7695169226c3b
Jack-42 <johnjack0987@gmail.com>
2024-11-21 11:48:21 -0700

    rename for clarity

--- sh_scripts/python/run_pubchem_assay_activities.sh
+++ sh_scripts/python/run_pubchem_assay_activities.sh
@@ -4,6 +4,6 @@ aid_file="${o_dir}/assay_set.aid"
 o_compound="${o_dir}/cpds_set.tsv"
 o_sid2cid="${o_dir}/sid2cid.tsv"
 o_assaystats="${o_dir}/assaystats.tsv"
-python3 src/pubchem_assays_local.py --aid_file $aid_file \
+python3 src/pubchem_assay_activities.py --aid_file $aid_file \
     --assay_zip_dir $assay_zip_dir --o_compound $o_compound \
     --o_sid2cid $o_sid2cid --o_assaystats $o_assaystats
