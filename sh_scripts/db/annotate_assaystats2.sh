fc989e3990a0f644cc119a65bace6e722816a345
Jack-42 <johnjack0987@gmail.com>
2024-11-22 15:01:11 -0700

    create new scripts for badapple2

---
+++ sh_scripts/db/annotate_assaystats2.sh
@@ -0,0 +1,64 @@
+# Author: Jack Ringer
+# Date: 11/22/2024
+# Description:
+# Annotate compound and scaffold tables with activity stats for Badapple2.
+# Yes, this is very similar to badapple1_compairson/sh_scripts/annotate_assaystats.sh
+# NOTE: This process can take several hours!
+
+if [ $# -lt 7 ]; then
+	printf "Syntax: %s DB_NAME DB_HOST SCHEMA ASSAY_ID_TAG DB_USER DB_PASSWORD REPO_DIR\n" $0
+	exit
+fi
+
+DB_NAME=$1
+DB_HOST=$2
+SCHEMA=$3
+ASSAY_ID_TAG=$4
+DB_USER=$5
+DB_PASSWORD=$6
+REPO_DIR=$7
+
+# cd to run scripts using relative path
+cd $REPO_DIR
+
+# Step 1) Generate compound activity statistics.  Populate/annotate
+# compound table with calculated assay stats.
+# (sTotal,sTested,sActive,aTested,aActive,wTested,wActive)
+psql -d $DB_NAME -c "UPDATE $SCHEMA.compound SET (nsub_total, nsub_tested, nsub_active)  = (NULL, NULL, NULL)"
+python src/annotate_db_assaystats.py \
+	--annotate_compounds \
+	--assay_id_tag $ASSAY_ID_TAG \
+	--host $DB_HOST \
+	--dbname $DB_NAME \
+	--schema $SCHEMA \
+	--activity $SCHEMA \
+	--user $DB_USER \
+	--password $DB_PASSWORD \
+	--v
+
+echo "Done annotating compounds."
+
+
+# Step 2) Generate scaf activity statistics.  Populate/annotate scaffold table with calculated assay stats.
+# Scaffold table must be ALTERed to contain activity statistics.
+# (cTotal,cTested,cActive,sTotal,sTested,sActive,aTested,aActive,wTested,wActive)
+psql -d $DB_NAME -c "UPDATE $SCHEMA.scaffold SET (ncpd_total, ncpd_tested, ncpd_active)  = (NULL, NULL, NULL)"
+psql -d $DB_NAME -c "UPDATE $SCHEMA.scaffold SET (nsub_total, nsub_tested, nsub_active)  = (NULL, NULL, NULL)"
+psql -d $DB_NAME -c "UPDATE $SCHEMA.scaffold SET (nass_tested, nass_active)  = (NULL, NULL)"
+psql -d $DB_NAME -c "UPDATE $SCHEMA.scaffold SET (nsam_tested, nsam_active)  = (NULL, NULL)"
+psql -d $DB_NAME -c "UPDATE $SCHEMA.scaffold SET in_drug  = NULL"
+
+
+python src/annotate_db_assaystats.py \
+	--annotate_scaffolds \
+	--assay_id_tag $ASSAY_ID_TAG \
+	--host $DB_HOST \
+	--dbname $DB_NAME \
+	--schema $SCHEMA \
+	--activity $SCHEMA \
+	--user $DB_USER \
+	--password $DB_PASSWORD \
+	--v \
+	--write_scafid2activeaid
+
+echo "Done annotating scaffolds."
---
+++ sh_scripts/db/annotate_scores2.sh
@@ -0,0 +1,66 @@
+# Author: Jack Ringer
+# Date: 11/22/2024
+# Description:
+# Generate scaffold scores + rankings using badapple formula for Badapple2 DB.
+
+if [ $# -lt 9 ]; then
+	printf "Syntax: %s DB_NAME DB_HOST SCHEMA ASSAY_ID_TAG DB_USER DB_PASSWORD REPO_DIR DATA_DIR DB_COMMENT\n" $0
+	exit
+fi
+
+DB_NAME=$1
+DB_HOST=$2
+SCHEMA=$3
+ASSAY_ID_TAG=$4
+DB_USER=$5
+DB_PASSWORD=$6
+REPO_DIR=$7
+DATA_DIR=$8
+DB_COMMENT=$9
+
+# make working dir to call scripts
+cd $REPO_DIR
+
+# Step 0) Generate assay id files
+ASSAY_ID_FILE=$DATA_DIR/${DB_NAME}_tested.${ASSAY_ID_TAG}
+psql -q --no-align -d $DB_NAME \
+	-c "SELECT DISTINCT ${ASSAY_ID_TAG} FROM $SCHEMA.activity ORDER BY ${ASSAY_ID_TAG}" \
+	|sed -e '1d' |sed -e '$d' \
+	>$ASSAY_ID_FILE
+
+ASSAY_ACTIVE_ID_FILE=$DATA_DIR/${DB_NAME}_active.${ASSAY_ID_TAG}
+psql -q --no-align -d $DB_NAME \
+	-c "SELECT DISTINCT ${ASSAY_ID_TAG} FROM $SCHEMA.activity WHERE outcome=2 ORDER BY ${ASSAY_ID_TAG}" \
+	|sed -e '1d' |sed -e '$d' \
+	>$ASSAY_ACTIVE_ID_FILE
+
+# Step 1) Load metadata.  Postgres 9.4+
+psql -d $DB_NAME -c "COMMENT ON DATABASE ${DB_NAME} IS '$DBCOMMENT'"
+bash badapple1_comparison/sh_scripts/db/metadata_update.sh $DB_NAME $SCHEMA $ASSAY_ID_FILE "$DB_COMMENT"
+
+# Step 2a) Annotate scaffold table with computed scores, add column "pscore".
+psql -d $DB_NAME -c "ALTER TABLE $SCHEMA.scaffold ADD COLUMN IF NOT EXISTS pscore FLOAT NULL"
+python src/annotate_db_scores.py \
+	--host $DB_HOST \
+	--dbname $DB_NAME \
+	--dbschema $SCHEMA \
+	--user $DB_USER \
+	--password $DB_PASSWORD \
+	-vvv
+
+# Step 2b) Annotate scaffold table with score rank, add column "prank".
+psql -d $DB_NAME -c "ALTER TABLE $SCHEMA.scaffold ADD COLUMN IF NOT EXISTS prank INT NULL"
+
+# Sub-select needed else: "ERROR: cannot use window function in UPDATE"
+psql -d $DB_NAME <<EOF
+UPDATE $SCHEMA.scaffold s
+SET prank = x.pr
+FROM (
+        SELECT id, rank() OVER (ORDER BY pscore DESC) AS pr
+        FROM $SCHEMA.scaffold s2
+        WHERE pscore IS NOT NULL
+        ) AS x
+WHERE s.id = x.id
+AND s.pscore IS NOT NULL
+        ;
+EOF
