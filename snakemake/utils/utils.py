4c4af033239d320dd39e8852573bc41376158ae1
Jack-42 <johnjack0987@gmail.com>
2024-11-22 12:46:04 -0700

    update config

--- snakemake/utils/utils.py
+++ snakemake/utils/utils.py
@@ -23,18 +23,27 @@ def update_config_paths(config: dict):
         config["ASSAY_DATA_DIR"], config["ASSAY_ID_FILE"]
     )
     config["BIOASSAY_FILE"] = os.path.join(
-        config["LOCAL_PUBCHEM_DIRECTORY"], "Bioassay", "Extras", "bioassays.tsv.gz"
+        config["LOCAL_PUBCHEM_DIR"], "Bioassay", "Extras", "bioassays.tsv.gz"
     )
     config["MIRROR_PUBCHEM_DONE_FILE"] = "logs/mirror_pubchem_records/job_done.txt"
 
     # for assay bioactivity data
     config["PUBCHEM_ASSAY_RECORDS_DIR"] = os.path.join(
-        config["LOCAL_PUBCHEM_DIRECTORY"], "Bioassay", "CSV", "Data"
+        config["LOCAL_PUBCHEM_DIR"], "Bioassay", "CSV", "Data"
     )
 
     # save misc assay data to assay dir, db tables to base dir
     for key in list(config.keys()):
         if key.startswith("ASSAY") and key != "ASSAY_DATA_DIR":
             config[key + "_PATH"] = os.path.join(config["ASSAY_DATA_DIR"], config[key])
-        elif key.endswith("TSV"):
+        elif key.endswith("TSV") and not (key.startswith("DRUG_CENTRAL")):
             config[key + "_PATH"] = os.path.join(config["BASE_DATA_DIR"], config[key])
+
+    # save downloaded PubChem file to PubChem dir
+    config["PUBCHEM_DATASOURCES_CSV_PATH"] = os.path.join(
+        config["LOCAL_PUBCHEM_DIR"], config["PUBCHEM_DATASOURCES_CSV"]
+    )
+    # save downloaded DrugCentral file to designated dir
+    config["DRUG_CENTRAL_TSV_PATH"] = os.path.join(
+        config["DRUG_CENTRAL_DIR"], config["DRUG_CENTRAL_TSV"]
+    )
