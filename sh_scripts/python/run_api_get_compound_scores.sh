0cde09940fcc9af567b18df37cb06153171d818d
Jack-42 <johnjack0987@gmail.com>
2024-11-26 11:20:06 -0700

    move api_scripts to outer dir

--- sh_scripts/python/run_api_get_compound_scores.sh
+++ sh_scripts/python/run_api_get_compound_scores.sh
@@ -1,11 +1,10 @@
 dir="/home/jack/unm_gra/tmp/dtra"
 input_tsv="${dir}/tsne2_head.csv"
 output_tsv="${dir}/tsne2_badapple_scores.tsv"
-python3 src/api_scripts/api_get_compound_scores.py \
+python3 src/api_get_compound_scores.py \
     --input_tsv "$input_tsv" \
     --output_tsv "$output_tsv" \
     --idelim "," \
     --iheader \
     --smiles_column 3 \
-    --name_column 2 \
-    --batch_size 2
+    --name_column 2
