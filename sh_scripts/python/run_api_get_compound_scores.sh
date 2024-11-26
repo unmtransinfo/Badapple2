1d1390abe1a3794462d4c6ceeabd594074e4ec35
Jack-42 <johnjack0987@gmail.com>
2024-11-26 12:18:30 -0700

    minor fix

--- sh_scripts/python/run_api_get_compound_scores.sh
+++ sh_scripts/python/run_api_get_compound_scores.sh
@@ -1,5 +1,5 @@
 dir="/home/jack/unm_gra/tmp/dtra"
-input_tsv="${dir}/tsne2_head.csv"
+input_tsv="${dir}/tsne2.csv"
 output_tsv="${dir}/tsne2_badapple_scores.tsv"
 python3 src/api_get_compound_scores.py \
     --input_tsv "$input_tsv" \
